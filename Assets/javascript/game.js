478a62dd823e0aebd1c718dbca83014197997d01
Jordan <daily@Jordans-MacBook.local>
2018-06-07 22:40:11 -0500

    Pseudo

---
+++ Assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+
+
+Generate a random number between 19 - 120, and assign it to the Target Number variable
+
+Generate  a random number between 1 - 12 and assign it to a crystal until each crystal is assigned a value that is not equal to zero. 
+
+Crystals: Connect each assigned value to the Total Score variable so that as 
+selections are made and buttons are pressed, the Total Score box tracks progression towards the Target Number. 
+
+Check for equality between the Total Score and the Target Score. 
+
+If the Total Score becomes equal to the Target Score, pregress to another round and increment the Wins value by +1
+
+If the Total Score exceeds the Target Number, reset the game and increment the Losses value by +1
+
